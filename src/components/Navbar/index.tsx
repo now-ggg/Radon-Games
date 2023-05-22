04d4d09d40081c11416919ef62dcc9168b622ed4
Ez <126034682+CodingHTML-Yusef@users.noreply.github.com>
2023-05-22 12:30:48 -0400

    Update index.tsx

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,10 +8,10 @@ export default function Navbar(): JSX.Element {
     <nav>
       <div class="flex items-center justify-center md:justify-between w-full sm:px-16 md:px-20 lg:px-32 shadow-2xl">
         <div class="flex gap-5 py-5">
-          <a href="/" title="Radon Games">
+          <a href="/" title="Glatic games">
             <img
               src="/icons/transparent.png"
-              alt="Radon Games"
+              alt="Glatic Games"
               class="h-6"
             ></img>
           </a>
@@ -22,7 +22,7 @@ export default function Navbar(): JSX.Element {
         <div class="hidden md:flex gap-5 items-center justify-center">
           <NavSearchbar />
           <IconButton
-            href="https://discord.gg/C2fbK35Rhg"
+            href="https://discord.gg/
             text="Discord"
             type="fa-brands"
             icon="fa-discord"
